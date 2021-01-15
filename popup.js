541b69d4958e86e9ee77838475ac3f68d66999d4
Ink3988 <44293641+Ink3988@users.noreply.github.com>
2021-01-15 15:17:29 +0000

    Update popup.js

--- popup.js
+++ popup.js
@@ -6,25 +6,3 @@ function chat(message){
     sendButton.dispatchEvent(new MouseEvent("click", { bubbles: true, cancelable: true, view: window }));
 }
 'use strict';
-// i copied this from the chrome extension samples, i am yet to work out all of it
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({number: 1}, function() {
-    console.log('The number is set to 1.');
-  });
-});
-
-function updateIcon() {
-  chrome.storage.sync.get('number', function(data) {
-    var current = data.number;
-    chrome.browserAction.setIcon({path: 'icon' + current + '.png'});
-    current++;
-    if (current > 5)
-      current = 1;
-    chrome.storage.sync.set({number: current}, function() {
-      console.log('The number is set to ' + current);
-    });
-  });
-};
-
-chrome.browserAction.onClicked.addListener(updateIcon);
-updateIcon();
